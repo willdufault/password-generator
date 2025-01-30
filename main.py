20fbf391fd346009a2529af85a1c0f9d63cbefe7
Will Dufault <99445180+willdufault@users.noreply.github.com>
2025-01-30 10:32:45 -0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,9 @@
+from random import choice
+
+CHARS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890!@#$"
+
+ln = 14
+cnt = 5
+
+for _ in range(cnt):
+    print("".join([choice(CHARS) for _ in range(ln)]))
